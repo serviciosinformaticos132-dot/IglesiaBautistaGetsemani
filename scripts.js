ee4dfbd9f3b28b8e0ac43db51060b0ae5f361dfb
serviciosinformaticos132-dot <serviciosinformaticos132@gmail.com>
2026-01-06 15:14:52 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ function mostrarSeccion(id) {
     secciones.forEach(sec => sec.classList.remove('activa'));
 
     document.getElementById(id).classList.add('activa');
+
+    // Cerrar submenu de Recursos al seleccionar opción
+    const submenu = document.getElementById("submenuRecursos");
+    if (submenu) submenu.style.display = "none";
+}
+
+function toggleRecursos() {
+    const submenu = document.getElementById("submenuRecursos");
+    submenu.style.display =
+        submenu.style.display === "block" ? "none" : "block";
 }
 
 // Año automático en el footer
